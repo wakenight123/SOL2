929e47923e577aa1cbbc32d62b13e8540b68cd06
github username <github email address>
2018-11-02 10:28:08 -0400

    added basic CSS to signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,14 @@ class UsersController < ApplicationController
       if logged_in?
         @user = current_user
         redirect_to user_path(@user)
+      else
+        render :new
       end
     end
 
     def create
       @user = User.create(user_params)
-      
+
       session[:user_id] = @user.id
 
       redirect_to experiences_path
