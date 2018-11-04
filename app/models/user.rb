a6ba61578e4abf04bfeb5d72eca10e79d38eaea2
github username <github email address>
2018-11-04 01:05:19 -0500

    added carrierwave and rmagick - not cropping

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-  
+  mount_uploader :avatar, AvatarUploader
+
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+  after_update :crop_avatar
+
+  def crop_avatar
+    avatar.recreate_versions! if crop_x.present?
+  end
+
   has_many :experiences
   has_many :participants, through: :experiences
 
